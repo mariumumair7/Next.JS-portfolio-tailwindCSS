be6a4db9ff5bf0d7c4b050da806abfbd661545f9
Marium Umair <160335280+mariumumair7@users.noreply.github.com>
2024-11-09 16:15:21 +0500

    Initial commit

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+export default function About(){
+    return(
+        <div className="contact">
+<p> Contact Number 0300-0000000</p>
+<p><a href="https://www.linkedin.com/in/marium-umair-2941a1215/">Linkedin</a></p>
+<p><a href="https://www.facebook.com">Facebook</a></p>
+
+
+        </div>
+    )
+}
